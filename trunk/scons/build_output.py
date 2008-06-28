64af0beae647bf5b7c2e446a05138493afe1b2f8
loonycyborg <loonycyborg@2307bb3d-1237-0410-822d-8b8cac1f6a97>
2008-06-28 20:50:47 +0000

    Made use of ECMA-48 escape codes in build output.

--- trunk/scons/build_output.py
+++ trunk/scons/build_output.py
@@ -1,17 +1,29 @@
 # vi: syntax=python:et:ts=4
+import sys
+
+colors = {
+    "blue"    : "\033[34m",
+    "yellow"  : "\033[33m",
+    "magenta" : "\033[35m",
+    "green"   : "\033[32m",
+    "bold"    : "\033[1m",
+    "nocolor" : "\033[0m"
+    }
 
 def setup_build_output(envs):
     for env in envs:
         if not env["VERBOSE_BUILD_OUTPUT"]:
-            env["CCCOMSTR"]     = "Compiling C source ${SOURCE.srcpath} ..."
-            env["CXXCOMSTR"]    = "Compiling C++ source ${SOURCE.srcpath} ..."
-            env["ARCOMSTR"]     = "Creating static library ${TARGET.srcpath} ..."
-            env["RANLIBCOMSTR"] = "Indexing ${TARGET.srcpath} ..."
-            env["LINKCOMSTR"]   = "Linking binary ${TARGET.srcpath} ..."
-            env["INSTALLSTR"]   = "Installing $SOURCE as $TARGET ..."
+            if sys.platform != "win32":
+                env.Replace(**colors)
+            env["CCCOMSTR"]     = "${blue}Compiling C source $bold${SOURCE.srcpath}$nocolor ..."
+            env["CXXCOMSTR"]    = "${blue}Compiling C++ source $bold${SOURCE.srcpath}$nocolor ..."
+            env["ARCOMSTR"]     = "${yellow}Creating static library $bold${TARGET.srcpath}$nocolor ..."
+            env["RANLIBCOMSTR"] = "${yellow}Indexing $bold${TARGET.srcpath}$nocolor ..."
+            env["LINKCOMSTR"]   = "${magenta}Linking binary $bold${TARGET.srcpath}$nocolor ..."
+            env["INSTALLSTR"]   = "Installing $bold$SOURCE$nocolor as $bold$TARGET$nocolor ..."
 
-            env["QT4_MOCCOMSTR"] = "Moccing ${SOURCE.srcpath} ..."
-            env["QT4_UICCOMSTR"]      = "Compiling user interface: ${SOURCE.srcpath} ..."
+            env["QT4_MOCCOMSTR"] = "${green}Moccing $bold${SOURCE.srcpath}$nocolor ..."
+            env["QT4_UICCOMSTR"] = "${green}Compiling user interface $bold${SOURCE.srcpath}$nocolor ..."
 
             env["NSISSCRIPTCOMSTR"] = "Generating NSIS script ..."
             env["MAKENSISCOMSTR"] = "Generating installer ..."
