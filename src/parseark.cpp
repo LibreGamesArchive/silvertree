d1543cd635c5640b6f868a252cfb58048aadc50c
loonycyborg <loonycyborg@2307bb3d-1237-0410-822d-8b8cac1f6a97>
2008-07-22 09:09:38 +0000

    Set both bone transform and inv bind matrix to identity in arkmodel parser.

--- src/parseark.cpp
+++ src/parseark.cpp
@@ -232,7 +232,7 @@ model_ptr parseark(const char* i1, const char* i2)
 
                 Eigen::MatrixP3f default_matrix;
                 default_matrix.loadIdentity();
-                default_matrix.setTranslationVector(default_pos);
+                //default_matrix.setTranslationVector(default_pos);
 
                 // TODO: pull out into quaternion class
                 const GLfloat sz1 = sinf(z1);
@@ -258,7 +258,7 @@ model_ptr parseark(const char* i1, const char* i2)
                 default_matrix(2,2) = 1 - 2*q_x*q_x - 2*q_y*q_y;
                 // end of quaternion horror
 
-                b.transform = default_matrix.matrix();
+                b.transform.loadIdentity();
                 b.inv_bind_matrix.loadIdentity();
                 
                 parents.insert(std::pair<std::string,int>(parent,bones.size()-1));
